6357c828a033026643f730caad0c7be5dc33a573
xSuui <samuel2012rogerio@gmail.com>
2024-01-15 16:09:07 -0300

    add recycle platforms

--- Infinity Runner/Assets/Scripts/SpawnPlatform.cs
+++ Infinity Runner/Assets/Scripts/SpawnPlatform.cs
@@ -4,24 +4,52 @@ using UnityEngine;
 
 public class SpawnPlatform : MonoBehaviour
 {
-    public List<GameObject> platforms = new List<GameObject>();
+    public List<GameObject> platforms = new List<GameObject>(); //lista dos prefabs das plataformas
+
+    private List<Transform> currentPlatforms = new List<Transform>(); //list de plataformas geradas na cena
+
+    private Transform player;
+    private Transform currentPlatformPoint;
+    private int platformIndex = 0;
 
     public float offset;
 
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
         for(int i = 0; i < platforms.Count; i++)
         {
-            Instantiate(platforms[i], new Vector2(i * 30, 0), transform.rotation);
+            Transform p = Instantiate(platforms[i], new Vector2(i * 30, 0), transform.rotation).transform;
+            currentPlatforms.Add(p);
             offset += 30f;
         }
 
+        currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().finalPoint;
     }
 
     
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        float distance = player.position.x - currentPlatformPoint.position.x;
+
+        if(distance >= 1)
+        {
+            Recycle(currentPlatforms[platformIndex].gameObject);
+            platformIndex++;
+
+            if(platformIndex > currentPlatforms.Count - 1)
+            {
+                platformIndex = 0;
+            }
+
+            currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().finalPoint;
+        }
     }
 
     public void Recycle(GameObject platform)
