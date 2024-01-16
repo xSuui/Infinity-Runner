32de7b9c27eb3f24151446bba571220ad59ae2fe
xSuui <samuel2012rogerio@gmail.com>
2024-01-16 15:27:33 -0300

    add more enemies

--- Infinity Runner/Assets/Scripts/SpawnEnemies.cs
+++ Infinity Runner/Assets/Scripts/SpawnEnemies.cs
@@ -29,6 +29,6 @@ public class SpawnEnemies : MonoBehaviour
 
     void SpawnEnemy()
     {
-        Instantiate(enemiesList[0], transform.position + new Vector3(0, Random.Range(0f, 3f), 0), transform.rotation);
+        Instantiate(enemiesList[Random.Range(0, enemiesList.Count)], transform.position + new Vector3(0, Random.Range(0f, 3f), 0), transform.rotation);
     }
 }
