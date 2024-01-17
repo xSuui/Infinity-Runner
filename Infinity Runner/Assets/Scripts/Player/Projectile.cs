7f5f98d6fe8b54f649022e2cd26fb3cf5d84ff24
xSuui <samuel2012rogerio@gmail.com>
2024-01-17 12:36:45 -0300

    add collision and damage to enemies

--- Infinity Runner/Assets/Scripts/Player/Projectile.cs
+++ Infinity Runner/Assets/Scripts/Player/Projectile.cs
@@ -7,6 +7,10 @@ public class Projectile : MonoBehaviour
     private Rigidbody2D rig;
     public float speed;
 
+    public int damage;
+
+    public GameObject expPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +19,23 @@ public class Projectile : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         rig.velocity = Vector2.right * speed;
+    }
+
+    public void OnHit()
+    {
+        GameObject e = Instantiate(expPrefab, transform.position, transform.rotation);
+        Destroy(e, 0.3f);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            OnHit();
+        }
     }
 }
